07c0ed3b38a954837afb9344e50ba6ea54d24d45
nickaldwin <nickaldwin>
2023-08-10 14:24:24 +0800

    making footer

--- views/components/Header.jsx
+++ views/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => (
 <Header className="text-stone-800">
     <div className="text-center">
         <h1 className="font-cherry lowercase text-4x1">
-            <a href="/profile">Bluebird.</a>
+            <a href="/profile">Login / Bluebird.</a>
         </h1>
         <span className="font-sans">Continue sharing your opinion on Bluebird</span> {/*i will add later*/} 
     </div>
