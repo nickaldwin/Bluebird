eb3665feef5cbe25f352852675bad63c8af27986
nickaldwin <nickaldwin>
2023-08-10 14:22:16 +0800

    making header ui

--- views/components/Header.jsx
+++ views/components/Header.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Header = () => (
 <Header className="text-stone-800">
-    
+    <div className="text-center">
+        <h1 className="font-cherry lowercase text-4x1">
+            <a href="/profile">Bluebird.</a>
+        </h1>
+        <span className="font-sans">Continue sharing your opinion on Bluebird</span> {/*i will add later*/} 
+    </div>
 </Header>
 );
 export default Header
